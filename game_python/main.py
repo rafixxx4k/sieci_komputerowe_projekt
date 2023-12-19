7e927f1f2fec3c4525023ebb1f228bbcd662fb10
StaGlo <stanley.glowczewski@gmail.com>
2023-12-19 21:54:24 +0100

    Add ip addr as arg

--- game_python/main.py
+++ game_python/main.py
@@ -80,16 +80,19 @@ def game_window(client_socket, me):
         clock.tick(30)
 
 
-def main():
+def main(ip):
     """
     The main function that initializes the client socket, handles login and starts the game.
+
+    Args:
+        ip (str): The IP address of the server.
 
     Returns:
         None
     """
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(("localhost", 1100))
+        client_socket.connect((ip, 1100))
         me = 1
         me = login_window(client_socket)
         game_window(client_socket, me)
@@ -99,5 +102,23 @@ def main():
         print(f"Błąd podczas połączenia z serwerem: {e}")
 
 
+def check_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+
+
 if __name__ == "__main__":
-    main()
+    # use ip address of the server as an argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <ip_address>")
+        sys.exit()
+
+    ip = sys.argv[1]
+    if not check_ip(ip):
+        print("Invalid ip address")
+        sys.exit()
+
+    main(ip)
